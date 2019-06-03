3c41e996dfec04fd6b4c025e7465a5f9d60a5141
silent-fly-fish <965302676@qq.com>
2019-06-03 13:39:19 +0800

    状态码修改

--- config/errMsg.php
+++ config/errMsg.php
@@ -12,8 +12,8 @@ return [
     'validationError' => ['code'=>500,'msg'=>'参数验证失败'],
     'InterfaceUrlError'=>['code'=>500,'msg'=>'接口地址未定义'],
     'InterfaceError'=>['code'=>500,'msg'=>'接口错误'],
-    'phoneIsRegister'=>['code'=>2001,'msg'=>'该手机号已注册'],
-    'phoneCodeError'=>['code'=>2002,'msg'=>'手机验证码错误'],
-    'phoneNotRegister'=>['code'=>2003,'msg'=>'该手机号尚未注册'],
-    'patientNotExist'=>['code'=>2004,'msg'=>'不存在该用户'],
+    'phoneIsRegister'=>['code'=>3001,'msg'=>'该手机号已注册'],
+    'phoneCodeError'=>['code'=>3002,'msg'=>'手机验证码错误'],
+    'phoneNotRegister'=>['code'=>3003,'msg'=>'该手机号尚未注册'],
+    'patientNotExist'=>['code'=>3004,'msg'=>'不存在该用户'],
 ];
