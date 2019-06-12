83988fc0a79b16a8c3e54fbc3b8a4aec3da94d66
huhaiyang <1045655109@qq.com>
2019-06-12 13:21:49 +0800

    后台举报功能

--- app/Http/Admin/PatientAccusationCtl.php
+++ app/Http/Admin/PatientAccusationCtl.php
@@ -13,6 +13,10 @@ use Illuminate\Http\Request;
 
 class PatientAccusationCtl
 {
+    /**
+     * 举报处理
+     * @param $data
+     */
     static function updateByID($data){
         $id = isset($data['id'])?$data['id']:0;
         $reason = isset($data['reason'])?$data['reason']:'';
@@ -28,6 +32,35 @@ class PatientAccusationCtl
         jsonOut('error',  $ret);
     }
 
+    /**
+     * 举报类型列表
+     * @param $postData
+     */
+    static function getTypeAll($postData){
+        $ret = [
+           [
+                'id'=>1,
+                'name'=>'文章'
+            ],
+            [
+                'id'=>2,
+                'name'=>'文章评论'
+            ],
+            [
+                'id'=>3,
+                'name'=>'帖子'
+            ],
+            [
+                'id'=>4,
+                'name'=>'帖子评论'
+            ],
+            [
+                'id'=>5,
+                'name'=>'医生评价'
+            ]
+        ];
+        jsonOut('success',  $ret);
+    }
 
     /**
      * 举报列表
@@ -36,9 +69,47 @@ class PatientAccusationCtl
     static function getAll($postData){
         $ret = PatientAccusationORM::getAll($postData);
 
+        $type = isset($postData['type'])?$postData['type']:0;
+//        print_r($ret);exit;
         if($ret['total'] > 0){
+            $accusationIds = array_unique(array_column($ret['list'],'accusation_id'));
+
+            if($type == 1){
+                $accusationArrJson =   GET('article.open/master',['article_ids'=>implode(',',$accusationIds)]);
+            }elseif($type == 2){
+                $accusationArrJson =   GET('article.open/comment',['comment_ids'=>implode(',',$accusationIds)]);
+            }elseif($type == 3){
+                $accusationArrJson =   GET('circle.open/master',['circle_ids'=>implode(',',$accusationIds)]);
+            }elseif($type == 4){
+                $accusationArrJson =   GET('circle.open/comment',['comment_ids'=>implode(',',$accusationIds)]);
+            }elseif($type == 5){
+                $accusationArrJson =   GET('question.open/evaluate',['evaluate_ids'=>implode(',',$accusationIds)]);
+            }
+
+            $accusationArrInfo = [];
             foreach ($ret['list'] as $k=>$v){
+                $accusationArrInfo = isset($accusationArrJson['data'][$v['accusation_id']])?$accusationArrJson['data'][$v['accusation_id']]:'';
+
+//                print_r($accusationArrJson['data'][$v['accusation_id']]);exit;
                 $ret['list'][$k]['created_at'] = $v['created_at']?date('Y-m-d',$v['created_at']):'';
+                $ret['list'][$k]['reason'] = $v['content']?$v['content']:'';
+                unset($ret['list'][$k]['content']);
+                if($type == 1){
+                    $ret['list'][$k]['title'] = isset($accusationArrInfo['title'])?$accusationArrInfo['title']:'';
+                    $ret['list'][$k]['sub_content'] = isset($accusationArrInfo['sub_content'])?$accusationArrInfo['sub_content']:'';
+                    $ret['list'][$k]['img_cover'] = isset($accusationArrInfo['img_cover'])?explode(';',$accusationArrInfo['img_cover'])[0]:'';
+                }elseif($type == 2){
+                    $ret['list'][$k]['sub_content'] = isset($accusationArrInfo['content'])?$accusationArrInfo['content']:'';
+                }elseif($type == 3){
+                    $ret['list'][$k]['title'] = isset($accusationArrInfo['title'])?$accusationArrInfo['title']:'';
+                    $ret['list'][$k]['content'] = isset($accusationArrInfo['content'])?$accusationArrInfo['content']:'';
+                    $ret['list'][$k]['img_cover'] = isset($accusationArrInfo['img_urls'])?explode(';',$accusationArrInfo['img_urls'])[0]:'';
+                }elseif($type == 4){
+                    $ret['list'][$k]['sub_content'] = isset($accusationArrInfo['content'])?$accusationArrInfo['content']:'';
+                }elseif($type == 5){
+                    $ret['list'][$k]['sub_content'] = isset($accusationArrInfo['evaluate'])?$accusationArrInfo['evaluate']:'';
+                }
+
             }
         }
         jsonOut('success',  $ret);
