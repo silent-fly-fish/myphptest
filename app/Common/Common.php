15240ee4ff5beabaff114da31ed7853393d98db0
silent-fly-fish <965302676@qq.com>
2019-07-22 16:36:09 +0800

    真实ip获取测试2

--- app/Common/Common.php
+++ app/Common/Common.php
@@ -437,7 +437,7 @@ function getRealIp() {
 
         for($i = 0; $i < count($ips); $i++){
 
-            if (!eregi ("^(10|172\.16|192\.168)\.", $ips[$i])){
+            if (!preg_match ("^(10|172\.16|192\.168)\.", $ips[$i])){
 
                 $ip = $ips[$i];
 
