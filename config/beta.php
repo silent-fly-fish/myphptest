5d765ef842f6a49944dcf08493f10a5c0c5a58f6
silent-fly-fish <965302676@qq.com>
2019-08-13 09:53:32 +0800

    线上环境部署

--- config/beta.php
+++ config/beta.php
@@ -15,15 +15,15 @@ if(App::environment('production')){
         //域名地址
         'SERVER_HOST'=>'https://dev.portal.futurefertile.com',
         //定义测试服上线ip，地址
-        'CIRCLE_SYS_IP'   =>'127.0.0.1:32090/index.php',
-        'ORDER_SYS_IP'    =>'127.0.0.1:32091/index.php',
-        'QUESTION_SYS_IP' =>'127.0.0.1:32092/index.php',
-        'ARTICLE_SYS_IP'  =>'127.0.0.1:32093/index.php',
-        'USER_SYS_IP'     =>'127.0.0.1:32094/index.php',
-        'INTERGRAL_SYS_IP'=>'127.0.0.1:32095/index.php',
+        'CIRCLE_SYS_IP'   =>'169.169.200.90:32090',
+        'ORDER_SYS_IP'    =>'169.169.200.62:8862',
+        'QUESTION_SYS_IP' =>'169.169.200.92:32092',
+        'ARTICLE_SYS_IP'  =>'169.169.200.93:32093',
+        'USER_SYS_IP'     =>'169.169.200.94:32094',
+        'INTERGRAL_SYS_IP'=>'169.169.200.95:32095',
         'INPUT_INVITE_ID' => 6, //输入验证码
         'LOGIN_ID' => 1, //登录
-        'TAG_SYS_IP'=>'127.0.0.1:32096/index.php',
+        'TAG_SYS_IP'=>'169.169.200.96:32096',
     ];
 }
 /**
